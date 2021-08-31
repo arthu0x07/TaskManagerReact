0fce788606b7087ce60a73739935be280c051112
Arthur Santos <arthur_bsggg@hotmail.com>
2021-08-31 17:34:56 -0300

    Projeto Estruturado

---
+++ src/View/Home/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './styles.js';
+
+class home extends React.Component{
+    constructor(props){
+        super(props)
+    }
+
+    render(){
+        return(
+            <h1> First Page </h1>
+        );
+    }
+}
+
+export default home;
